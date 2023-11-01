306b27ad5a5122ae46d170672e82c8f0dae628cf
RagnarBob <makendyalexis5414@gmail.com>
2023-11-01 10:21:36 -0400

    the king

---
+++ cours3.py
@@ -0,0 +1,11 @@
+"""def get_name(**kwargs):
+    user = input(f"user {kwargs['i']}  :")
+    print(user.lower(),(kwargs.get('laj'),"ans"))
+    #print(kwargs.get('laj'),"ans")
+get_name(i=1,laj=3)
+get_name(i=2,laj=4)
+get_name(i=3,laj=7)"""
+
+print_name = lambda i : print(i)
+print_name(2)
+
